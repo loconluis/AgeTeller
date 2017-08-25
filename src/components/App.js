0925a6ff8c234e4659cd1193eb169dd6afc5f68a
luislocon <luis.locon@gmail.com>
2017-08-24 20:14:48 -0600

    Adding state

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,32 @@ import { Form, FormControl, Button } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      newDate: '',
+      birthday: '1992-06-21',
+    }
+  }
+
+  changeBirthday(){
+    console.log(this.state);
+    this.setState({ birthday: this.state.newDate });
+  }
+
   render() {
     return (
       <div className="App">
         <Form inline>
           <h2>Input Your Birthday!</h2>
-          <FormControl type="date">
+          <FormControl
+            type="date"
+            onChange={event => this.setState({ newDate: event.target.value })}
+          >
           </FormControl>
           {' '}
-          <Button>
+          <Button onClick={() => this.changeBirthday()} >
             Submit
           </Button>
         </Form>
